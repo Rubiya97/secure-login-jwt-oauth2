6d7ced855dd8cbddea89bc197a987a7d49c12fc1
Rubiya97 <rubiyasultana97@gmail.com>
2026-01-20 22:44:47 -0500

    Update User.java

--- oauth2/src/main/java/rubiya/oauth2/entity/User.java
+++ oauth2/src/main/java/rubiya/oauth2/entity/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "\"user\"")
